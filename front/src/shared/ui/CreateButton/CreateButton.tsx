49fd7af3f244934fe97ace977971ceac75686cd6
Alexey917 <pochtovii.1996@mail.ru>
2025-11-23 14:53:37 +0300

    Добавил изменение цвета к кнопке

--- front/src/shared/ui/CreateButton/CreateButton.tsx
+++ front/src/shared/ui/CreateButton/CreateButton.tsx
@@ -1,9 +1,14 @@
+import { useSelector } from 'react-redux';
+import type { RootState } from '@/shared/lib/store';
+
 import classes from './CreateButton.module.css';
 import plus from '../../assets/sprite.svg';
 
 export const CreateButton = () => {
+  const colorTheme = useSelector((state: RootState) => state.colorTheme.color);
+
   return (
-    <button type="button" className={classes.btn}>
+    <button type="button" className={`${classes.btn} ${classes[colorTheme]}`}>
       <div className={classes.topLine}></div>
       <div className={classes.rightLine}></div>
       <div className={classes.bottomLine}></div>
