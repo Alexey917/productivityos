5dfa67c72f63d35f9c9c941ea29b65946715deda
Alexey917 <pochtovii.1996@mail.ru>
2025-12-16 01:12:32 +0300

    добавил модель для юзера

--- back/users/models.py
+++ back/users/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  class sexChoices(models.TextChoices):
+    MALE = 'мужчина', _('Мужчина')
+    MALE = 'женщина', _('Женщина')
+    NO_SPECIFIED = 'не указано', _('Не указано')
+
+  username = models.CharField(max_length=20, unique=True, verbose_name='Логин', help_text=_('От 3 до 20 символов. Только буквы, цифры и .-_'))
+  email = models.EmailField(blank=True, null=True, verbose_name=_('Email'), default=None)
+  first_name = models.CharField(blank=True, null=True, verbose_name=_('Имя'), default=None)
+  last_name = models.CharField(blank=True, null=True, verbose_name=_('Фамилия'), default=None)
+  photo_url = models.URLField(blank=True)
+  birthday = models.DateField(blank=True, null=True, verbose_name=_('Дата рождения'), default=None, help_text='Формат: ГГГГ-ММ-ДД')
+  city = models.CharField(blank=True, null=True, verbose_name=_('Город'), default=None, max_length=100)
+  sex = models.CharField(blank=True, verbose_name=_('Пол'), default=sexChoices.NO_SPECIFIED, choices=sexChoices.choices)
+  create_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Дата создания'))
+  update_at = models.DateTimeField(auto_now=True, verbose_name=_('Дата обновления'))
+
+  class Meta: 
+    verbose_name = _('Пользователь')
+    verbose_name_plural = _('Пользователи')
+    ordering = ['-create_at']
+
+    indexes = [
+      models.Index(fields=['username']),
+      models.Index(fields=['email']),
+      models.Index(fields=['create_at']),
+    ] 
+
+    def __str__(self):
+      if self.first_name and self.last_name:
+        return f'{self.first_name} {self.last_name}'
+      return self.username or str(self.pk)
